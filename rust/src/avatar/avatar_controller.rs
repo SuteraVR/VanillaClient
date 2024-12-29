e90fbda344a7caac5a382c684709e15e874efc57
LegitYew0108 <legitlegit0108@gmail.com>
2024-12-29 20:11:27 +0900

    fix: make sutera avatar controller's base node xrorigin3d

--- rust/src/avatar/avatar_controller.rs
+++ rust/src/avatar/avatar_controller.rs
@@ -2,36 +2,29 @@ use godot::prelude::*;
 use godot::classes::*;
 
 #[derive(GodotClass)]
-#[class(base=XrCamera3D)]
+#[class(base=XrOrigin3D)]
 pub struct AvatarController {
-    base: Base<XrCamera3D>,
+    base: Base<XrOrigin3D>,
     right_controller: Option<Gd<XrController3D>>,
     left_controller: Option<Gd<XrController3D>>,
-    xr_origin: Option<Gd<XrOrigin3D>>,
+    xr_camera: Option<Gd<XrCamera3D>>,
     speed: f32,
 }
 
 #[godot_api]
-impl IXrCamera3D for AvatarController {
-    fn init(base: Base<XrCamera3D>) -> Self {
+impl IXrOrigin3D for AvatarController {
+    fn init(base: Base<XrOrigin3D>) -> Self {
         Self {
             base,
             right_controller: None,
             left_controller: None,
-            xr_origin: None,
+            xr_camera: None,
             speed: 2.5,
         }
     }
 
     fn enter_tree(&mut self) {
-        godot_print!("enter_tree");
-        let Some(xr_origin_parent) = self.base_mut().get_parent() else{
-            godot_print!("No parent node found");
-            return;
-        };
-        self.xr_origin = Some(xr_origin_parent.clone().cast::<XrOrigin3D>());
-
-        let children = xr_origin_parent.get_children();
+        let children = self.base_mut().get_children();
 
         self.right_controller = children.iter_shared().find(|child| {
             child.get_name() == StringName::from("RightController")
@@ -40,22 +33,26 @@ impl IXrCamera3D for AvatarController {
         self.left_controller = children.iter_shared().find(|child| {
             child.get_name() == StringName::from("LeftController")
         }).map(|child| child.cast::<XrController3D>());
+
+        self.xr_camera = children.iter_shared().find(|child| {
+            child.get_name() == StringName::from("XrCamera")
+        }).map(|child| child.cast::<XrCamera3D>());
     }
 
     fn process(&mut self, delta: f64) {
+
         let left_controller_clone = self.left_controller.clone();
-        left_controller_clone.map(|controller|{
+        if let Some(controller) = left_controller_clone{
             let stick_vector = controller.get_vector2("primary".into());
             let speed = self.speed;
-            let mut xr_origin = self.xr_origin.clone().unwrap();
-            xr_origin.translate(Vector3::new(-1.0 * stick_vector.x * speed * delta as f32, 0.0, stick_vector.y * speed * delta as f32));
-        });
+            self.base_mut().translate(Vector3::new(-1.0 * stick_vector.x * speed * delta as f32, 0.0, stick_vector.y * speed * delta as f32));
+        }
+
         let right_controller_clone = self.right_controller.clone();
-        right_controller_clone.map(|controller|{
+        if let Some(controller) = right_controller_clone{
             let stick_vector = controller.get_vector2("primary".into());
             let speed = self.speed;
-            let mut xr_origin = self.xr_origin.clone().unwrap();
-            xr_origin.rotate_y(-stick_vector.x * speed * delta as f32);
-        });
+            self.base_mut().rotate_y(-stick_vector.x * speed * delta as f32);
+        }
     }
 }
