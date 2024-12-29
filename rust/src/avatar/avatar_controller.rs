c9644fc30ea13453477fcadd00045487fb0f090f
LegitYew0108 <legitlegit0108@gmail.com>
2024-12-29 19:43:28 +0900

    fix: Camera position and order

--- rust/src/avatar/avatar_controller.rs
+++ rust/src/avatar/avatar_controller.rs
@@ -2,26 +2,36 @@ use godot::prelude::*;
 use godot::classes::*;
 
 #[derive(GodotClass)]
-#[class(base=XrOrigin3D)]
+#[class(base=XrCamera3D)]
 pub struct AvatarController {
-    base: Base<XrOrigin3D>,
+    base: Base<XrCamera3D>,
     right_controller: Option<Gd<XrController3D>>,
     left_controller: Option<Gd<XrController3D>>,
+    xr_origin: Option<Gd<XrOrigin3D>>,
+    speed: f32,
 }
 
 #[godot_api]
-impl IXrOrigin3D for AvatarController {
-    fn init(base: Base<XrOrigin3D>) -> Self {
+impl IXrCamera3D for AvatarController {
+    fn init(base: Base<XrCamera3D>) -> Self {
         Self {
             base,
             right_controller: None,
             left_controller: None,
+            xr_origin: None,
+            speed: 2.5,
         }
     }
 
     fn enter_tree(&mut self) {
         godot_print!("enter_tree");
-        let children = self.base_mut().get_children();
+        let Some(xr_origin_parent) = self.base_mut().get_parent() else{
+            godot_print!("No parent node found");
+            return;
+        };
+        self.xr_origin = Some(xr_origin_parent.clone().cast::<XrOrigin3D>());
+
+        let children = xr_origin_parent.get_children();
 
         self.right_controller = children.iter_shared().find(|child| {
             child.get_name() == StringName::from("RightController")
@@ -36,13 +46,16 @@ impl IXrOrigin3D for AvatarController {
         let left_controller_clone = self.left_controller.clone();
         left_controller_clone.map(|controller|{
             let stick_vector = controller.get_vector2("primary".into());
-
-            self.base_mut().translate(Vector3::new(stick_vector.x*delta as f32, 0.0, stick_vector.y*delta as f32));
+            let speed = self.speed;
+            let mut xr_origin = self.xr_origin.clone().unwrap();
+            xr_origin.translate(Vector3::new(-1.0 * stick_vector.x * speed * delta as f32, 0.0, stick_vector.y * speed * delta as f32));
         });
         let right_controller_clone = self.right_controller.clone();
         right_controller_clone.map(|controller|{
             let stick_vector = controller.get_vector2("primary".into());
-            self.base_mut().rotate_y(-stick_vector.x*delta as f32);
+            let speed = self.speed;
+            let mut xr_origin = self.xr_origin.clone().unwrap();
+            xr_origin.rotate_y(-stick_vector.x * speed * delta as f32);
         });
     }
 }
